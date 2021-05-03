631aa982d4c44ea783484997064bd1ea8940fc07
dmitry <dmitri.blyus@gmail.com>
2021-05-04 02:01:40 +0300

    Use another name for reserve mp3 files

--- src/Handler.js
+++ src/Handler.js
@@ -3,6 +3,8 @@ const ReserveTextToMP3 = require('./ReserveTextToMP3');
 const MP3ToWAV = require('./MP3ToWAV');
 const path = require('path');
 
+const RESERVED_FILE_PREFIX = '!reserved~';
+
 // TODO Retry policy
 // TODO Use cache
 // TODO Rate limit
@@ -58,27 +60,35 @@ class Handler {
     const text = variables.agi_arg_1;
     console.log('text = ', text);
 
-    const filename = text.replace(/[\/\\"']/g, '--');
-    const filepath = path.join(this._recordsDir, filename);
-    console.log('Create MP3 using Google Text-To-Speech API', filename);
-    const mp3Filepath = filepath + '.mp3';
+    let filepath;
     try {
-      await this._advancedTextToMP3.convert(text, mp3Filepath);
+      console.log('Create MP3 using Google Text-To-Speech API');
+      filepath = await this._createMP3(text);
     } catch (error) {
-      console.error(error);
-      console.log('Create MP3 using Google Translate API');
-      await this._reserveTextToMP3.convert(text, mp3Filepath);
+      console.log('Create WAV using Google Translate API');
+      filepath = await this._createReserveMP3(text);
     }
 
-    const wavFilepath = filepath + '.wav';
-    console.log('Convert MP3 to WAV', wavFilepath);
-    await this._mp3ToWAV.convert(mp3Filepath, wavFilepath);
+    console.log('Convert MP3 to WAV', filepath);
+    await this._mp3ToWAV.convert(filepath + '.mp3', filepath + '.wav');
 
     console.log('Stream file');
     await this._context.streamFile(filepath, '#');
 
     console.log('Finish');
     await this._context.end();
+  }
+
+  async _createMP3(text) {
+    const filepath = path.join(this._recordsDir, text.replace(/[\/\\"']/g, '--'));
+    await this._advancedTextToMP3.convert(text, filepath + '.mp3');
+    return filepath;
+  }
+
+  async _createReserveMP3(text) {
+    const filepath = path.join(this._recordsDir, RESERVED_FILE_PREFIX + text.replace(/[\/\\"']/g, '--'));
+    await this._reserveTextToMP3.convert(text, filepath + '.mp3');
+    return filepath;
   }
 }
 
