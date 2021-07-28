129b0c78c22cd358801e8a57442aaf173019b23d
dmitry <dmitri.blyus@gmail.com>
2021-07-29 00:55:50 +0300

    No recreate MP3 if exists

--- src/Handler.js
+++ src/Handler.js
@@ -1,27 +1,17 @@
-const TextToMP3 = require('./TextToMP3');
-const MP3ToWAV = require('./MP3ToWAV');
-const path = require('path');
-const retry = require('./utils/retry');
-const isFileExists = require('./utils/isFileExists');
-const crypto = require('crypto');
+const TextToWAV = require('./TextToWAV');
 
 const RESULT_VAR = 'TEXT_TO_SPEECH_RESULT';
-const TEXT_TO_SPEECH_RETRY = 3;
 
 class Handler {
-  constructor(context, textToMP3, mp3ToWAV, recordsDir) {
+  constructor(context, textToWAV) {
     this._context = context;
-    this._textToMP3 = textToMP3;
-    this._mp3ToWAV = mp3ToWAV;
-    this._recordsDir = recordsDir;
+    this._textToWAV = textToWAV;
   }
 
   static create(context, recordsDir, voiceName) {
     return new Handler(
       context,
-      TextToMP3.create(voiceName),
-      MP3ToWAV.create(),
-      recordsDir
+      TextToWAV.create(recordsDir, voiceName, true)
     );
   }
 
@@ -62,19 +52,7 @@ class Handler {
       console.log(`Set variable ${RESULT_VAR}=FAILED`);
       await this._context.setVariable(RESULT_VAR, 'FAILED');
 
-      console.log('Create MP3 using Google Text-To-Speech API');
-      const hash = crypto.createHash('md5').update(text).digest('hex');
-      const filepath = path.join(this._recordsDir, hash);
-      if (!await isFileExists(filepath + '.wav')) {
-        await retry(async () => {
-          return await this._textToMP3.convert(text, filepath + '.mp3');
-        }, TEXT_TO_SPEECH_RETRY);
-
-        console.log('Convert MP3 to WAV', filepath);
-        await this._mp3ToWAV.convert(filepath + '.mp3', filepath + '.wav');
-      } else {
-        console.log(`File ${filepath}.wav exists`);
-      }
+      const {filepath} = await this._textToWAV.convert(text);
 
       console.log('Stream file');
       await this._context.streamFile(filepath, '#');
---
+++ src/TextToWAV.js
@@ -0,0 +1,61 @@
+const crypto = require('crypto');
+const path = require('path');
+const isFileExists = require('./utils/isFileExists');
+const retry = require('./utils/retry');
+const TextToMP3 = require('./TextToMP3');
+const MP3ToWAV = require('./MP3ToWAV');
+
+const TEXT_TO_SPEECH_RETRY = 3;
+
+class TextToWAV {
+  constructor(recordsDir, _isFileExists, textToMP3, mp3ToWAV, debug = false) {
+    this._recordsDir = recordsDir;
+    this._isFileExists = _isFileExists;
+    this._textToMP3 = textToMP3;
+    this._mp3ToWAV = mp3ToWAV;
+    this._debug = debug;
+  }
+
+  static create(recordsDir, voiceName, debug) {
+    return new TextToWAV(
+      recordsDir,
+      isFileExists,
+      TextToMP3.create(voiceName),
+      MP3ToWAV.create(),
+      debug
+    );
+  }
+
+  async convert(text) {
+    const hash = crypto.createHash('md5').update(text).digest('hex');
+    const filepath = path.join(this._recordsDir, hash);
+    const wavExists = await this._isFileExists(filepath + '.wav');
+
+    if (!wavExists && !await this._isFileExists(filepath + '.mp3')) {
+      this._log('Create MP3 using Google Text-To-Speech API');
+      await retry(async () => {
+        return await this._textToMP3.convert(text, filepath + '.mp3');
+      }, TEXT_TO_SPEECH_RETRY);
+    }
+
+    if (!wavExists) {
+      this._log('Convert MP3 to WAV', filepath);
+      await this._mp3ToWAV.convert(filepath + '.mp3', filepath + '.wav');
+    } else {
+      this._log(`File ${filepath}.wav exists`);
+    }
+
+    return {
+      filepath,
+      file: filepath + '.wav'
+    };
+  }
+
+  _log(...args) {
+    if (this._debug) {
+      console.log(...args);
+    }
+  }
+}
+
+export default TextToWAV;
